66d04bfd5ccf19ca7740fdba692b9da9856f5e9a
forzeyy <fxrzeyy@gmail.com>
2024-10-29 20:33:00 +0300

    routes for login + register

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/forzeyy/idea-shop-api/handlers"
+	"github.com/forzeyy/idea-shop-api/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -10,20 +11,22 @@ func hello(c *fiber.Ctx) error {
 }
 
 func SetupRoutes(app *fiber.App) {
+	authHandler := handlers.NewAuthHandler()
 	productHandler := handlers.NewProductHandler()
 	userHandler := handlers.NewUserHandler()
 
 	api := app.Group("/api")
 	api.Get("/", hello)
-	//products routes
+
+	api.Post("/login", authHandler.Login)
+	api.Post("/register", authHandler.Register)
+
 	api.Get("/products", productHandler.GetAllProducts)
 	api.Get("/products/:id", productHandler.GetProductByID)
-	api.Post("/products", productHandler.CreateProduct)
-	api.Put("/products/:id", productHandler.UpdateProduct)
-	api.Delete("/products/:id", productHandler.DeleteProduct)
-	// user routes
-	api.Get("/users", userHandler.GetAllUsers)
-	api.Get("/users/:id", userHandler.GetUserByID)
-	api.Put("/users/:id", userHandler.UpdateUser)
-	api.Delete("/users/:id", userHandler.DeleteUser)
+
+	protected := api.Group("/protected", middleware.Protected())
+
+	// more protected routes later
+	protected.Put("/users/:id", userHandler.UpdateUser)
+	protected.Delete("/users/:id", userHandler.DeleteUser)
 }
